c285c1167a9a607c2ca81dc6c9fb7073aa628410
Joseph Sintum <sjumbam15@gmail.com>
2017-06-11 22:57:01 +0100

    Create index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/master.css" media="screen" charset="utf-8">
-    <link rel="stylesheet" href="css/bootstrap.css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>Xplora | Chat and Share</title>
   </head>
 
